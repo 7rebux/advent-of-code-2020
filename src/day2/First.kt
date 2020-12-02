e509238686440024c8ee87c0d13195790d111709
7Rebux <imrebux@gmail.com>
2020-12-02 20:08:24 +0100

    initial commit

--- src/day2/First.kt
+++ src/day2/First.kt
@@ -1,9 +1,33 @@
 package day2
 
-/**
- * Created by Nils Osswald at Dezember 02, 2020
- * Project: aoc-2020
- *
- * @author Nils Osswald
- * @version 1.0
- */
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun main() {
+    val database = Files.readAllLines(Paths.get("assets/day2.txt"))
+    var valid = 0
+
+    for (entry in database) {
+        if (isValid(entry))
+            valid++
+    }
+
+    print(valid)
+}
+
+fun isValid(entry: String): Boolean {
+    val args = entry.split(" ")
+    var count = 0
+
+    val min = args[0].split("-")[0].toInt()
+    val max = args[0].split("-")[1].toInt()
+    val char = args[1].replace(":", "").single()
+    val password = args[2]
+
+    for (c in password) {
+        if (c == char)
+            count++
+    }
+
+    return count in min..max
+}
--- src/day2/Second.kt
+++ src/day2/Second.kt
@@ -1,9 +1,34 @@
 package day2
 
-/**
- * Created by Nils Osswald at Dezember 02, 2020
- * Project: aoc-2020
- *
- * @author Nils Osswald
- * @version 1.0
- */
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun main() {
+    val database = Files.readAllLines(Paths.get("assets/day2.txt"))
+    var valid = 0
+
+    for (entry in database) {
+        if (isValid(entry))
+            valid++
+    }
+
+    print(valid)
+}
+
+fun isValid(entry: String): Boolean {
+    val args = entry.split(" ")
+    var count = 0
+
+    val pos = args[0].split("-").map { it.toInt() }
+    val char = args[1].replace(":", "").single()
+    val password = args[2]
+
+    for (i in pos) {
+        if (i <= password.length) {
+            if (password.elementAt(i-1) == char)
+                count++
+        }
+    }
+
+    return count == 1
+}
