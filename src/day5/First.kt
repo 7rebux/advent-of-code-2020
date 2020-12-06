5dddf7ff142d91d70c7c0b064494c31a2df23c85
7Rebux <imrebux@gmail.com>
2020-12-06 01:33:41 +0100

    added day 5

---
+++ src/day5/First.kt
@@ -0,0 +1,23 @@
+package day5
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun main() {
+    val seats = Files.readAllLines(Paths.get("assets/day5.txt"))
+    var id = 0
+
+    for (line in seats) {
+        val s = line.replace('B', '1')
+            .replace('F', '0')
+            .replace('R', '1')
+            .replace('L', '0')
+
+        val row = Integer.parseInt(s.toCharArray(0, 7).concatToString(), 2)
+        val col = Integer.parseInt(s.toCharArray(7, 10).concatToString(), 2)
+
+        id = maxOf(id, row * 8 + col)
+    }
+
+    print(id)
+}
---
+++ src/day5/Second.kt
@@ -0,0 +1,26 @@
+package day5
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun main() {
+    val seats = Files.readAllLines(Paths.get("assets/day5.txt"))
+    val ids = arrayListOf<Int>()
+
+    for (line in seats) {
+        val s = line.replace('B', '1')
+            .replace('F', '0')
+            .replace('R', '1')
+            .replace('L', '0')
+
+        val row = Integer.parseInt(s.toCharArray(0, 7).concatToString(), 2)
+        val col = Integer.parseInt(s.toCharArray(7, 10).concatToString(), 2)
+
+        ids.add(row * 8 + col)
+    }
+
+    for (id in ids.sorted()) {
+        if ((id+1) !in ids && (id+2) in ids)
+            print(id+1)
+    }
+}
