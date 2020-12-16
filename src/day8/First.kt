d2c12e603eae3445452258e3e220e0fcf5faf944
7Rebux <imrebux@gmail.com>
2020-12-16 17:47:00 +0100

    added day 8 first task

---
+++ src/day8/First.kt
@@ -0,0 +1,30 @@
+package day8
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun main() {
+    val input = Files.readAllLines(Paths.get("assets/day8.txt"))
+    val commands = arrayOf<List<String>>().toMutableList()
+
+    input.forEach {
+        commands.add(it.split(" "))
+    }
+
+    var accumulator = 0
+    val seen = emptyArray<Int>().toMutableList()
+
+    var i = 0
+    while (true) {
+        if (i >= commands.size || i in seen)
+            break
+        seen.add(i)
+        when (commands[i][0]) {
+            "nop" -> ++i
+            "acc" -> { accumulator += commands[i][1].toInt(); ++i }
+            "jmp" -> i += commands[i][1].toInt()
+        }
+    }
+
+    print(accumulator)
+}
